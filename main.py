5a68c183075614805b9817206a728e1321ac1d21
rita-pr <rgperezromero@gmail.com>
2025-09-23 22:50:22 -0600

    Creación de main, y colocación de caratúla en main

---
+++ main.py
@@ -0,0 +1,10 @@
+# %% ==================================================
+# Banco de Guatemala
+# Septiembre 2025
+#
+# Proyecto: caja registradora para café "El Economista"
+#
+#Integrantes:
+# Chuvac González, Berni Alejandro
+# Pérez Romero, Rita Guadalupe
+# =====================================================
