4652da4bade53f41bab28e2bb80d4668e36f8080
Chuvi00 <bernimes84@gmail.com>
2025-09-24 00:35:39 -0600

    Primera version para importar y leer archivos desde "catologo"

--- data_io.py
+++ data_io.py
@@ -1,1 +1,25 @@
 # Anotaciones Berni
+# ================================================================================ --- IGNORE ---
+#Esto nos permitirá importar y leer archivos CSV
+import csv 
+# Para poder manejar rutas de archivos de manera más sencilla
+from pathlib import Path
+
+carpeta_catalogo = Path("datos/catalogos")
+
+#Definir la primera función para leer catálogo de bebidas
+def leer_bebidas():
+
+# Creamos la ruta completa al archivo CSV
+    ruta = carpeta_catalogo / "bebidas_menu.csv"
+
+# Abrimos el archivo y leemos su contenido y lo almacenamos en un diccionario en f 
+    with open(ruta, newline="", encoding='utf-8-sig') as f:
+        # leer el archivo CSV y indicar que el delimitador es una coma
+        archivo = csv.DictReader(f, delimiter=',')
+        print(archivo.fieldnames)  # Imprime los nombres de las columnas
+        bebidas = []
+        for fila in archivo:
+            
+            bebidas.append(fila)
+    return bebidas
